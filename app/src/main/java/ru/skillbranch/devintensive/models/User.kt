c39d61f8efcfaebe3be874dfa3c95fda09c1e8e2
Vladimir <znayka2005@gmail.com>
2019-06-28 00:22:36 +0300

    hometask 2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,54 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+class User(
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String?,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    var lastVisit: Date? = null,
+    var isOnline: Boolean = false
+) {
+    constructor(id: String, firstName: String?, lastName :String?) : this(
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    //constructor(id:String) : this(id, "Johh", "Doe")
+
+    init{
+        println("It's Alive!!!\n" +
+                "${if(lastName==="Doe") "His name is $firstName $lastName " else "End his name is $firstName $lastName!!!"}\n")
+    }
+
+
+    fun printMe() = println("""
+            id: $id
+            firstName: $firstName
+            lastName: $lastName
+            avatar: $avatar
+            rating: $rating
+            respect: $respect
+            lastVisit: $lastVisit
+            isOnline: $isOnline
+        """.trimIndent())
+
+    companion object Factory {
+        private var lastId : Int = -1
+        fun makeUser(fullname:String?) : User{
+            lastId++
+
+            val parts : List<String>? = fullname?.split(" ")
+
+            var firstName =parts?.getOrNull(0)
+            var lastName =parts?.getOrNull(1)
+            return User(id= "$lastId", firstName = firstName,lastName = lastName)
+        }
+    }
+
+}
