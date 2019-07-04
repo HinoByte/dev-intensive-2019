22e979d6b538529e83e58ecb90f91209ca1fb061
Vladimir <znayka2005@gmail.com>
2019-07-04 13:11:23 +0300

    add new 2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -34,59 +34,53 @@ fun Date.humanizeDiff(date: Date = Date()): String {
 
     var Diff = this.time - date.time
     var time =""
-    if (Diff<=1)
+    var postfix =""
+    var prefix = ""
+    if (Diff<=1) postfix = " назад" else prefix = "через "
+
     when (abs(Diff) / SECOND) {
         in 0..1 -> time = "только что"
-        in 1..45 -> time = "несколько секунд назад"
-        in 45..75 -> time = "минуту назад"
-        in 75..45 * Minute -> time = "${minutesAsPlulars(abs(Diff / MINUTE))} назад"
-        in 45 * Minute..75 * Minute -> time = "час назад"
-        in 76 * Minute..22 * Hour -> time = "${hoursAsPlulars(abs(Diff/ HOUR))} назад"
-        in 22 * Hour..26 * Hour -> time = "день назад"
-        in 26 * Hour..360 * Day -> time = "${daysAsPlulars(abs(Diff/ DAY))} назад"
-        else -> time = "более года назад"
+        in 1..45 -> time =  prefix+"несколько секунд$postfix"
+        in 45..75 -> time = prefix+"минуту$postfix"
+        in 75..45 * Minute -> time = "$prefix${TimeUnits.MINUTE.plural(abs(Diff / MINUTE))}$postfix"
+        in 45 * Minute..75 * Minute -> time = prefix+"час$postfix"
+        in 76 * Minute..22 * Hour -> time = "$prefix${TimeUnits.HOUR.plural(abs(Diff / HOUR))}$postfix"
+        in 22 * Hour..26 * Hour -> time = prefix+"день$postfix"
+        in 26 * Hour..360 * Day -> time = "$prefix${TimeUnits.DAY.plural(abs(Diff / DAY))}$postfix"
+        else -> if (Diff<=1) time = "более года назад" else time ="более чем через год"
     }
-    else when (abs(Diff) / SECOND) {
-        in 0..1 -> time = "только что"
-        in 1..45 -> time = "через несколько секунд"
-        in 45..75 -> time = "через минуту"
-        in 75..45 * Minute -> time = "через ${minutesAsPlulars(abs(Diff/ MINUTE))}"
-        in 45 * Minute..75 * Minute -> time = "через час"
-        in 76 * Minute..22 * Hour -> time = "через ${hoursAsPlulars(abs(Diff/ HOUR))}"
-        in 22 * Hour..26 * Hour -> time = "через день"
-        in 26 * Hour..360 * Day -> time = "через ${daysAsPlulars(abs(Diff/ DAY))}"
-        else -> time = "более чем через год"
-    }
+    //"$prefix${getTimeUnitValue(timeDiffUnsigned, TimeUnits.MINUTE)}$postfix"
     return time
 }
 
-enum class TimeUnits{
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+enum class TimeUnits {
+    SECOND{
+        override fun plural(value : Long) = when (value.asPlulars){
+            Plulars.ONE -> "$value секунду"
+            Plulars.FEW -> "$value секунды"
+            Plulars.MANY-> "$value секунд"}},
+    MINUTE {
+        override fun plural(value: Long): String = when (value.asPlulars){
+            Plulars.ONE -> "$value минуту"
+            Plulars.FEW -> "$value минуты"
+            Plulars.MANY-> "$value минут"}},
+    HOUR{
+        override fun plural(value : Long) : String= when (value.asPlulars){
+            Plulars.ONE -> "$value час"
+            Plulars.FEW -> "$value часа"
+            Plulars.MANY-> "$value часов"}},
+    DAY{
+        override fun plural(value : Long) : String= when (value.asPlulars){
+            Plulars.ONE -> "$value день"
+            Plulars.FEW -> "$value дня"
+            Plulars.MANY-> "$value дней"}};
+    abstract fun plural (value : Long) : String
 }
 
 enum class Plulars{
     ONE,
     FEW,
     MANY
-}
-
-private fun minutesAsPlulars(value: Long) = when (value.asPlulars){
-    Plulars.ONE -> "$value минуту"
-    Plulars.FEW -> "$value минуты"
-    Plulars.MANY-> "$value минут"
-}
-private fun hoursAsPlulars(value: Long) = when (value.asPlulars){
-    Plulars.ONE -> "$value час"
-    Plulars.FEW -> "$value часа"
-    Plulars.MANY-> "$value часов"
-}
-private fun daysAsPlulars(value: Long) = when (value.asPlulars){
-    Plulars.ONE -> "$value день"
-    Plulars.FEW -> "$value дня"
-    Plulars.MANY-> "$value дней"
 }
 
 val Long.asPlulars
@@ -96,4 +90,27 @@ val Long.asPlulars
         this % 10L in 2L..4L -> Plulars.FEW
         else -> Plulars.MANY
     }
+
+fun String.truncate(value:Int = 16):String {
+    val new= this
+    if (new.length <  value) return new.trim()
+    return new.substring(0,value).trim()+"..."
+}
+
+//val String.stripHtml
+//    get() = {
+//        var translitLetters = mapOf<String, String>("а" to "a",
+//            "&amp" to "b",
+//            )
+//        val value = this
+//        var rezult = ""
+//        value.forEach {
+//            val symbol = translitLetters.getOrDefault(it.toLowerCase().toString(), it.toString())
+//            rezult += if (it.isUpperCase()) symbol.capitalize() else symbol
+//        }
+//    }
+
+
+
+
 //"$prefix${getTimeUnitValue(timeDiffUnsigned, TimeUnits.MINUTE)}$postfix"
